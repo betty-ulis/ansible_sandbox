4224fbf82d26ce1b9f2d6bcbceb2f88865cd7301
betty <uvetki@gmail.com>
2026-03-07 20:24:35 +0200

    MAIN-000: use module for check of existing tools

--- library/do_tools_exist.py
+++ library/do_tools_exist.py
@@ -1,41 +1,46 @@
 import shutil
 
+from ansible.module_utils.basic import AnsibleModule
+
 
 def run(module: AnsibleModule):
     results = {
         "check_name": "required tools exist",
     }
+    details = {}
     try:
         tool_list = module.params["tool_list"]
+        details["all_tools"] = tool_list
+
         missing_tools = []
         for tool in tool_list:
-            if not shutil.which(tool):
+            # shutil.which is marked deprecated only for PathLike on Windows <3.12.
+            # We pass a string and run on Linux, so this is safe.
+            # noinspection PyDeprecation
+            if not shutil.which(tool):   # pylint: disable=deprecated-method
                 missing_tools.append(tool)
 
         if missing_tools:
+            details["missing_tools"] = missing_tools
             module.exit_json(
                 **results,
                 status="failed",
                 check_errors=f"Missing required tools: {', '.join(missing_tools)}",
-                check_details={
-                    "missing_tools": missing_tools,
-                    "all_tools": tool_list
-                }
+                check_details=details
             )
         else:
             module.exit_json(
                 **results,
                 status="success",
-                check_details={
-                    "all_tools": tool_list
-                }
+                check_details=details
             )
     except Exception as e:
+        details["check_errors"] = str(e)
         module.exit_json(
             **results,
             status="error",
             check_errors=str(e),
-            check_details={"details": "Unexpected error during check execution"}
+            check_details=details
         )
 
 def main():
